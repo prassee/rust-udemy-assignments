6c3409c3882019a1172e4b353ce06c31e9e1f4eb
Prasanna Kumar <prassee@prassee.me>
2019-11-18 08:33:01 +0530

    added more code examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let isvowl = vowels_finder('x');
+    println!("{}", isvowl);
+    basic_calculator(23, 32, '+');
+}
+
+fn vowels_finder(letter: char) -> bool {
+    if letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' {
+        true
+    } else {
+        false
+    }
+}
+
+fn basic_calculator(a: i32, b: i32, oper: char) {
+    let result = match oper {
+        '+' => a + b,
+        '-' => a - b,
+        '*' => a * b,
+        '/' => a / b,
+        _ => a + b,
+    };
+    println!("{}", result);
 }
